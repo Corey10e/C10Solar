a646c1dc205f9a43bfccacdee61c462edcff7acf
Corey Tenney <corey.tenney@outlook.com>
2016-04-28 19:38:45 -0600

    Initial Checkin

---
+++ C10SolarWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(C10SolarWeb.Startup))]
+namespace C10SolarWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
